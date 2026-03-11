3a9ab143fe84b8001616caa1a0b63e0563d66f85
marcocentofanti <marcocentofanti@hotmail.it>
2026-03-11 19:54:15 +0100

    Start main structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import { useState } from 'react'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+
+
+import Homepage from './pages/Homepage';
+import Products from './pages/Products';
+import About from './pages/About';
+import DefaultLayout from './layouts/DefaultLayout';
+
+
 
 function App() {
 
@@ -10,7 +18,11 @@ function App() {
     <>
      <BrowserRouter>
      <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route element={<DefaultLayout/>}>
+          <Route path='/' element={<Homepage/>} />
+          <Route path='/Product' element={<Products/>} />
+          <Route path='/About' element={<About/>} />
+        </Route>
       </Routes>
      </BrowserRouter>
     </>
